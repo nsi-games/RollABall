43f15f3a8e59042c66381967676b7044dc6b48d7
Manny Vaccaro <emmanuel.vaccaro@gmail.com>
2019-04-14 11:19:01 +1000

    Initial commit

---
+++ Assets/Scripts/Collectable/Collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RollABall
+{
+    public class Collectable : MonoBehaviour
+    {
+        public int value = 1;
+
+        public void Collect()
+        {
+            GameManager.Instance.AddScore(value);
+            // Temp
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RollABall
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Singleton
+        public static GameManager Instance = null;
+        private void Awake()
+        {
+            Instance = this;
+        }
+        private void OnDestroy()
+        {
+            Instance = null;
+        }
+        #endregion
+
+        public int score = 0;
+        public bool isGameRunning = true;
+        public Transform collectables;
+        [Header("UI")]
+        public Text scoreText;
+
+        void Update()
+        {
+            // If the game is not running
+            if (!isGameRunning)
+                return; // Exit Update
+
+            // Are all collectables gone?
+            if(collectables.childCount == 0)
+            {
+                // Game is no longer running
+                isGameRunning = false;
+                // Game Over
+                GameOver();
+            }
+        }
+
+        void GameOver()
+        {
+
+        }
+
+        public void AddScore(int scoreToAdd)
+        {
+            score += scoreToAdd;
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+}
